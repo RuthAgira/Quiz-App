8725344e5e17a6b109a5cd369c1f4e162fa55d4a
RuthAgira <agiraruth@gmail.com>
2024-01-31 14:53:14 +0300

    name

--- app.js
+++ app.js
@@ -1,120 +1,179 @@
-const questions =[
+const questions = [
     {
-        question: "How do you write 'Hello World' in an alert box?"
+        question: "Java script is an ------------ language?" ,
         answers: [
-           {text: alertBox('Hello World'), correct: true},
-           {text: alert('Hello World'), correct: false},
-           {text: msg('Hello World'), correct: false},
-           {text: msgBox('Hello World'), correct:false},
+        { text: "Object-oriented", correct: true},
+        { text: "Object-Based" , correct: false},
+        { text: "Procedural", correct: false},
+        { text: "None of the above", correct:false},
         ]
     },
     {
-        question: "What is the correct way to write a javaScript array?"
+        question: "How can a datatype be declared to be a constant type?",
         answers: [
-           { text: "var colors" = "red", "green", "blue", correct: false},
-           { text: "var colors"["red","green", "blue"],  correct: true},
-           { text: "var=1 =("red"),2=("green"), 3= ("blue")", correct: false},
-           { text: "var colors (1:"red"), (2:"green"), (3:"blue")", correct:false},
+           { text: "const", correct: true},
+           { text: "var",  correct: false},
+           { text: "let", correct: false},
+           { text: "constant", correct:false},
         ]
 
     },
     {
-        question: "How do round the number 7.25, to the nearest interger?"
+        question: "Which of the following keywords is used to define a variable in javascript?",
         answers: [
-           {text: "round(7.25)",  correct: false},
-           {text: "math.round(7.25)", correct: true},
-           {text: "md(7.25)", correct: false},
-           {text: "math.rnd(7.25)", correct:false},
+           { text: "var",  correct: true},
+           { text: "let", correct: false},
+           { text: "Both A and B", correct: false},
+           { text: "None of the above", correct:false},
         ]
     },
     {
-        question: "JavaScript is the programming language of the _"
+        question: "JavaScript is the programming language of the _",
         answers: [
-           {text: 'Desktop', correct: false},
-           {text: 'Mobile', correct: false},
-           {text: 'Web', correct: true},
-           {text: 'Server',  correct:false},
+           { text: "Desktop", correct: false},
+           { text: "Mobile", correct: false},
+           { text: "Web", correct: true},
+           { text: "Server",  correct:false},
         ]
     },
     {
-        question: "How does FOR loop start?"
+        question: "What key word is used to check whether a given property is valid or not?",
         answers: [
-           {text: "for(i=0;i<5)", correct: false},
-           {text: "for(i=0; i<5; i++", correct: true},
-           {text: "for i=1-5", correct: false},
-           {text: "for(i<5; i++)",  correct:false},
+           { text: " is in", correct: false},
+           { text: "in", correct: true},
+           { text: "exists", correct: false},
+           { text: "lies",  correct:false},
         ]
     },
     {
-        question: "Which operator is used to sign a variable?"
+        question: "Which of the following is not a Jvascript framework?",
         answers: [
-           {text: "-",  correct: false},
-           {text: "=", correct: true},
-           {text: "*",  correct: false},
-           {text: "+",  correct:false},
+           { text: "Node",  correct: false},
+           { text: "Cassandra", correct: true},
+           { text: "Vue",  correct: false},
+           { text: "React",  correct:false},
         ]
     },
     {
-        question: "Which event occours when the user clicks on an HTML element?"
+        question: "Which event occours when the user clicks on an HTML element?",
         answers: [
-           {text: "onmouseover",  correct: false},
-           {text: "onmouseclick", correct: false},
-           {text: "onchange", correct: false},
-           {text: "onclick", correct:true},
+           { text: "onmouseover",  correct: false},
+           { text: "onmouseclick", correct: false},
+           { text: "onchange", correct: false},
+           { text: "onclick", correct:true},
         ]
     },
     {
-        question: "How do you find the number with the highest value of x and y?"
+        question: "Which of the following are closures in Javascript?",
         answers: [
-           {text: "top(x,y)", correct: false},
-           {text: "math.ceil(x,y)",  correct: false},
-           {text: " math.max(x,y)", correct: true},
-           {text: "cei(x,y)", correct:false},
+           { text: "Variables", correct: false},
+           { text: "Functions",  correct: false},
+           { text: "Objects", correct: true},
+           { text: "All the above", correct:false},
         ]
     },
     {
-        question: "How to write an IF statement in javascript?"
+        question: "When an operator's value is NULL, the type of returned by the unary operator is?",
         answers: [
-           {text:  "if i = 5 then", correct: false},
-           {text:  "if i ==5 then", correct: false},
-           {text: "if i = 5",  correct: false},
-           {text: "if (i==5)",  correct:true},
+           { text:  "Boolean", correct: false},
+           { text:  "Undefined", correct: false},
+           { text: "Integer",  correct: false},
+           { text: "Object",  correct:true},
         ]
     },
     {
-        question: "Inside which HTML element do we put javaScript?"
+        question: "Inside which HTML element do we put javaScript?",
         answers: [
-           {text: "scripting", correct: false},
-           {text: "js", correct: false},
-           {text:  "javascript", correct: false},
-           {text: "script", correct:true},
+           { text: "scripting", correct: false},
+           { text: "js", correct: false},
+           { text:  "javascript", correct: false},
+           { text: "script", correct:true},
         ]
     }
 ];
 
 const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-buttons");
+const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
 
-function  Quiz App(){
+function  startQuiz(){
     currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = "Next";
-    showQustion();
+    showQuestion();
 }
-function showQustion(){
-    let currentQuestion = questions[QuestionsIndex];
+    function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + "." currentQuestion.question;
+    questionElement.innerHTML = questionNo + " . "  + currentQuestion.question;
 
     currentQuestion.answers.forEach(answer => {
-        const button = document.createElement('button');
+        const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
-        answerButton.appendChild(button;)
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+
     });
+    }
+
+    function selectAnswer(e){
+        const selectedBtn = e.target;
+        const isCorrect = selectedBtn.dataset.correct === "true";
+        if(isCorrect){
+            selectedBtn.classList.add("correct");
+            score++;
+        }else{
+            selectedBtn.classList.add("incorrect");
+        }
+    }
+        Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+
+    });
+    nextButton.style.display = "block";
+
+    function showScore (){
+    resetState();
+    questionElement.innerHTML = 'You Scored ${score} out of $ {questions.length}!';
+    nextButton.innerHTML = "play again";
+    nextButton.style.display = "block";
+}
+
+    function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQustion();
+     }else{
+        showScore();
+     }
     
 }
+    nextButton.addEventListener("click", () =>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+
+    }
+});
+
+    function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild); 
+ }
+}
+
+
+startQuiz();
